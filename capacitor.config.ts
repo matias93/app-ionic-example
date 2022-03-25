5e7e534edd26e4a6fb7457428bf806fb7d044f55
Matias Arturo Gonzalez Roa <maty.gr93@gmail.com>
2022-03-25 12:42:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'operation-geozone',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
